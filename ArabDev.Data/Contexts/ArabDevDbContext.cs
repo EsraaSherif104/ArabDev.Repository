dceb78f9c3a8153bf51fd472802a1f2f0f7f73a6
Esraa <esraasherif1042@gmail.com>
2025-02-21 23:30:12 +0200

    model

---
+++ ArabDev.Data/Contexts/ArabDevDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ArabDev.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabDev.Data.Contexts
+{
+    internal class ArabDevDbContext :IdentityDbContext<User>
+    {
+    }
+}
